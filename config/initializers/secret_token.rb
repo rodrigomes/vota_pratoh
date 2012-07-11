c7045773ed3a786906320d15d3d0d6fdf922fd32
elogroup <rgomes.rodrigo@gmail.com>
2012-07-11 15:26:56 -0300

    projeto criado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VotaPratoh::Application.config.secret_token = 'dde5bfd88b8dc4ebe52d60bb0e25dc53d1ddc11b564c5643da0235a4883be4a808c75fc03dc0287e4c72e46a88e27deefc6042eea1898b8f0c6edbc028380867'
