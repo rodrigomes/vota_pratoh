6955e16c37a0dea78d86a398d4e1235b6fe66737
elogroup <elogroup@elogroup-Dell-System-Vostro-3450.(none)>
2012-07-12 14:16:17 -0300

    modificacao no arquivo de modelo de qualificacao

--- app/models/qualificacao.rb
+++ app/models/qualificacao.rb
@@ -1,3 +1,15 @@
 class Qualificacao < ActiveRecord::Base
   # attr_accessible :title, :body
+  validates_presence_of :nota,
+    :message => " - deve ser preenchido"
+  validates_presence_of :valor_gasto,
+    :message => " - deve ser preenchido"
+  validates_numericality_of :nota,
+    :greater_than_or_equal_to => 0,
+    :less_than_or_equal_to => 10,
+    :message => " - deve ser um número entre 0 e 10"
+  validates_numericality_of :valor_gasto,
+    :greater_than => 0,
+    :message => " - deve ser um número maior que 0"
+
 end
