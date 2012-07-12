1e197119c6295d5c18bc5f64bdd1ee4e1de26c4d
elogroup <elogroup@elogroup-Dell-System-Vostro-3450.(none)>
2012-07-12 15:01:07 -0300

    restaurante has many qualificacoes, has and belongs to many pratos

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -10,12 +10,13 @@ class Restaurante < ActiveRecord::Base
   validates_uniqueness_of :endereco, :message => "endereço já cadastrado"
 
   validate :primeira_letra_deve_ser_maiuscula
+ 
+  has_many :qualificacoes  
+  has_and_belongs_to_many :pratos
   
-  private
+private
   def primeira_letra_deve_ser_maiuscula
     errors.add(:nome, "primeira letra deve ser maiúscula")
     unless nome =~ /[A-Z].*/ 
   end
-
-
 end
