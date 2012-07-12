7687d91142c2be47349154934dc551b06587940b
elogroup <elogroup@elogroup-Dell-System-Vostro-3450.(none)>
2012-07-12 15:11:57 -0300

    prato has and belongs to many restaurantes, and has one receita

--- app/models/prato.rb
+++ app/models/prato.rb
@@ -3,5 +3,8 @@ class Prato < ActiveRecord::Base
   validates_presence_of :nome, :message => " - deve ser preenchido"
   
   validates_uniqueness_of :nome, :message => " - nome já cadastrado"
+  
+  has_and_belongs_to_many :restaurantes
+  has_one :receita
 
 end
