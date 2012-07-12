fad0e3743d0af585eb49c179b4b8b05422cc9123
elogroup <elogroup@elogroup-Dell-System-Vostro-3450.(none)>
2012-07-12 11:51:35 -0300

    adicionei validacao da classe prato

--- app/models/prato.rb
+++ app/models/prato.rb
@@ -1,3 +1,7 @@
 class Prato < ActiveRecord::Base
   # attr_accessible :title, :body
+  validates_presence_of :nome, :message => " - deve ser preenchido"
+  
+  validates_uniqueness_of :nome, :message => " - nome já cadastrado"
+
 end
