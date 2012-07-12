433cdc10bef74489eef145beab673a892e47e9f9
elogroup <elogroup@elogroup-Dell-System-Vostro-3450.(none)>
2012-07-12 13:04:37 -0300

    executar o rake db:migrate para o qualificacoes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120712145247) do
+ActiveRecord::Schema.define(:version => 20120712153648) do
 
   create_table "clientes", :force => true do |t|
     t.string   "nome",       :limit => 80
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(:version => 20120712145247) do
     t.datetime "created_at",               :null => false
     t.datetime "updated_at",               :null => false
   end
+
+  create_table "qualificacoes", :force => true do |t|
+    t.integer  "cliente_id"
+    t.integer  "restaurante_id"
+    t.float    "nota"
+    t.float    "valor_gasto"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  add_index "qualificacoes", ["cliente_id"], :name => "index_qualificacoes_on_cliente_id"
+  add_index "qualificacoes", ["restaurante_id"], :name => "index_qualificacoes_on_restaurante_id"
 
   create_table "receita", :force => true do |t|
     t.integer  "prato_id"
