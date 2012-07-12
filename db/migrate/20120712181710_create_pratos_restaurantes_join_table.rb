c90c51f783cce86dfb6545351b95010b22738f5d
elogroup <elogroup@elogroup-Dell-System-Vostro-3450.(none)>
2012-07-12 15:33:56 -0300

    tirando o id da tabela joint entre prato e restaurante, e editando o create table migration

--- db/migrate/20120712181710_create_pratos_restaurantes_join_table.rb
+++ db/migrate/20120712181710_create_pratos_restaurantes_join_table.rb
@@ -1,5 +1,8 @@
 class CreatePratosRestaurantesJoinTable < ActiveRecord::Migration
   def up
+    create_table :pratos_restaurantes, :id => false do |t|
+    t.integer :prato_id
+    t.integer :restaurante_id
   end
 
   def down
