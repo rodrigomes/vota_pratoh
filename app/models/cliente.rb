cc183a54b5efa5d9101dec1e7a23aadeb2eadb43
elogroup <elogroup@elogroup-Dell-System-Vostro-3450.(none)>
2012-07-12 11:35:32 -0300

    adicionei validation para clientes

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,14 @@
 class Cliente < ActiveRecord::Base
   # attr_accessible :title, :body
+  validates_presence_of :nome,
+    :message => " - deve ser preenchido"
+  
+  validates_uniqueness_of :nome,
+    :message => " - nome já cadastrado"
+  
+  validates_numericality_of :idade,
+    :greater_than => 0,
+    :less_than => 100,
+    :message => " - deve ser um número entre 0 e 100"
+
 end
