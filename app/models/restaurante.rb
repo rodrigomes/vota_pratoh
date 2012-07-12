bd917dfe23662611ee0524ae4f83538971ffa6ac
elogroup <rgomes.rodrigo@gmail.com>
2012-07-12 11:04:12 -0300

    adicionei validates presence e uniqueness no restaurante model

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,3 +1,12 @@
+#encoding: utf-8
+
 class Restaurante < ActiveRecord::Base
-  # attr_accessible :title, :body
+  
+  validates_presence_of :nome, :message => "deve ser preenchido"
+  validates_presence_of :endereco, :message => "deve ser preenchido"
+  validates_presence_of :especialidade, :message => "deve ser preenchido"
+  
+  validates_uniqueness_of :nome, :message => "nome já cadastrado"
+  validates_uniqueness_of :endereco, :message => "endereço já cadastrado"
+
 end
