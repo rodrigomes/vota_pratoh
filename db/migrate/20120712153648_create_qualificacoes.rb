e97cc6510d07db64fe665a702c6c36f133888a69
elogroup <elogroup@elogroup-Dell-System-Vostro-3450.(none)>
2012-07-12 12:59:25 -0300

    adicionar linhas em migration de qualificacoes

--- db/migrate/20120712153648_create_qualificacoes.rb
+++ db/migrate/20120712153648_create_qualificacoes.rb
@@ -1,7 +1,10 @@
 class CreateQualificacoes < ActiveRecord::Migration
   def change
     create_table :qualificacoes do |t|
-
+      t.integer :cliente_id
+      t.integer :restaurante_id
+      t.float :nota
+      t.float :valor_gasto
       t.timestamps
     end
   end
